a21231b30e7abe8b09a09dbe148556cf69d88293
udykas <udykas@gmail.com>
2018-07-03 13:05:12 -0400

    Added new and imporved assets, moved platforms to match new foreground

--- public/scenes/game.js
+++ public/scenes/game.js
@@ -16,10 +16,12 @@ class MainGameScene extends Scene {
   }
 
   preload() {
+    this.load.image('starry-night', './assets/imgs/starry-night.png');
     this.load.image('background', './assets/imgs/background.png');
     this.load.image('foreground', './assets/imgs/gc-buildings.png');
     this.load.image('void', './assets/imgs/Solid_black.png');
-    this.load.image('ground', './assets/imgs/platform.png');
+    this.load.image('ground', './assets/imgs/blk-ground.png');
+    this.load.image('platform', './assets/imgs/sml-platform.png');
     this.load.image('batarang', './assets/imgs/batarang.png');
     this.load.spritesheet('enemy', './assets/imgs/enemy.png', { frameWidth: 48, frameHeight: 55 });
     this.load.spritesheet('stand', './assets/imgs/stand2.png', { frameWidth: 41.8, frameHeight: 55 });
@@ -33,8 +35,9 @@ class MainGameScene extends Scene {
   }
 
   create() {
-    this.add.image(1411, 150, 'background');
-    this.add.image(1411, 410, 'foreground');
+    this.add.image(1411, 185, 'starry-night');
+    this.add.image(1411, 310, 'background');
+    this.add.image(1411, 390, 'foreground');
 
     player = this.physics.add.sprite(100, 450, 'stand').setScale(1.15);
     player.setCollideWorldBounds(true);
@@ -95,50 +98,66 @@ class MainGameScene extends Scene {
     platforms = this.physics.add.staticGroup();
 
     //FLOOR
-    platforms.create(300, 1120, 'ground').setScale(15).refreshBody();
+    platforms.create(300, 600, 'ground').setScale(5).refreshBody();
 
     //FIRST PLATFORMS
-    platforms.create(200, 440, 'ground').setScale(0.25).refreshBody();
-    platforms.create(300, 410, 'ground').setScale(0.25).refreshBody();
-    platforms.create(400, 410, 'ground').setScale(0.25).refreshBody();
-    platforms.create(450, 410, 'ground').setScale(0.25).refreshBody();
-    platforms.create(500, 410, 'ground').setScale(0.25).refreshBody();
+    
+    platforms.create(290, 450, 'platform').setScale(0.65).refreshBody();
+    platforms.create(390, 450, 'platform').setScale(0.65).refreshBody();
+    platforms.create(440, 450, 'platform').setScale(0.65).refreshBody();
+    
 
     //SECOND PLATFORMS
-    platforms.create(700, 300, 'ground').setScale(0.25).refreshBody();
-    platforms.create(750, 300, 'ground').setScale(0.25).refreshBody();
-    platforms.create(800, 300, 'ground').setScale(0.25).refreshBody();
-    platforms.create(850, 300, 'ground').setScale(0.25).refreshBody();
-    platforms.create(900, 300, 'ground').setScale(0.25).refreshBody();
+    platforms.create(550, 330, 'platform').setScale(0.25).refreshBody();
+    platforms.create(600, 330, 'platform').setScale(0.25).refreshBody();
+    platforms.create(650, 330, 'platform').setScale(0.25).refreshBody();
+    platforms.create(700, 330, 'platform').setScale(0.25).refreshBody();
+    platforms.create(750, 330, 'platform').setScale(0.25).refreshBody();
+    platforms.create(800, 330, 'platform').setScale(0.25).refreshBody();
+    platforms.create(850, 330, 'platform').setScale(0.25).refreshBody();
 
-    platforms.create(1090, 150, 'ground').setScale(0.25).refreshBody();
+    platforms.create(950, 450, 'platform').setScale(0.65).refreshBody();
+    platforms.create(1100, 450, 'platform').setScale(0.65).refreshBody();
+
+    platforms.create(1080, 200, 'platform').setScale(0.65).refreshBody();
 
     //THIRD PLATFORMS
-    platforms.create(1275, 300, 'ground').setScale(0.25).refreshBody();
-    platforms.create(1325, 300, 'ground').setScale(0.25).refreshBody();
-    platforms.create(1375, 300, 'ground').setScale(0.25).refreshBody();
+    platforms.create(1275, 350, 'platform').setScale(0.25).refreshBody();
+    platforms.create(1325, 350, 'platform').setScale(0.25).refreshBody();
+    platforms.create(1375, 350, 'platform').setScale(0.25).refreshBody();
+
+    platforms.create(1475, 200, 'platform').setScale(0.25).refreshBody();
+    platforms.create(1525, 200, 'platform').setScale(0.25).refreshBody();
+    platforms.create(1575, 200, 'platform').setScale(0.25).refreshBody();
+    platforms.create(1625, 200, 'platform').setScale(0.25).refreshBody();
    
 
     //FOURTH PLATFORMS
-    platforms.create(1700, 300, 'ground').setScale(0.25).refreshBody();
-    platforms.create(1750, 300, 'ground').setScale(0.25).refreshBody();
-    platforms.create(1800, 300, 'ground').setScale(0.25).refreshBody();
-    platforms.create(1850, 300, 'ground').setScale(0.25).refreshBody();
-    platforms.create(1900, 300, 'ground').setScale(0.25).refreshBody();
+    platforms.create(1450, 450, 'platform').setScale(0.25).refreshBody();
+    platforms.create(1500, 450, 'platform').setScale(0.25).refreshBody();
+    platforms.create(1550, 450, 'platform').setScale(0.25).refreshBody();
+    platforms.create(1600, 450, 'platform').setScale(0.25).refreshBody();
+    platforms.create(1650, 450, 'platform').setScale(0.25).refreshBody();
+    platforms.create(1700, 450, 'platform').setScale(0.25).refreshBody();
+    platforms.create(1750, 450, 'platform').setScale(0.25).refreshBody();
+    platforms.create(1800, 450, 'platform').setScale(0.25).refreshBody();
+    platforms.create(1850, 450, 'platform').setScale(0.25).refreshBody();
+    platforms.create(1900, 450, 'platform').setScale(0.25).refreshBody();
 
-    platforms.create(2110, 430, 'ground').setScale(0.25).refreshBody();
+    platforms.create(2110, 450, 'platform').setScale(0.25).refreshBody();
+
+    platforms.create(2110, 330, 'platform').setScale(0.5).refreshBody();
 
     //FINAL PLATFORMS
-    platforms.create(2150, 175, 'ground').setScale(0.25).refreshBody();
-    platforms.create(2200, 175, 'ground').setScale(0.25).refreshBody();
-    platforms.create(2250, 175, 'ground').setScale(0.25).refreshBody();
-    platforms.create(2300, 175, 'ground').setScale(0.25).refreshBody();
-    platforms.create(2350, 175, 'ground').setScale(0.25).refreshBody();
-    platforms.create(2400, 175, 'ground').setScale(0.25).refreshBody();
-    platforms.create(2450, 175, 'ground').setScale(0.25).refreshBody();
-    platforms.create(2500, 175, 'ground').setScale(0.25).refreshBody();
-    platforms.create(2550, 175, 'ground').setScale(0.25).refreshBody();
-    platforms.create(2600, 175, 'ground').setScale(0.25).refreshBody();
+    
+    platforms.create(2250, 175, 'platform').setScale(0.25).refreshBody();
+    platforms.create(2300, 175, 'platform').setScale(0.25).refreshBody();
+    platforms.create(2350, 175, 'platform').setScale(0.25).refreshBody();
+    platforms.create(2400, 175, 'platform').setScale(0.25).refreshBody();
+    platforms.create(2450, 175, 'platform').setScale(0.25).refreshBody();
+    platforms.create(2500, 175, 'platform').setScale(0.25).refreshBody();
+    platforms.create(2550, 175, 'platform').setScale(0.25).refreshBody();
+    platforms.create(2600, 175, 'platform').setScale(0.25).refreshBody();
     
     //COLLECTABLES
     batarang = this.physics.add.group({
@@ -148,7 +167,7 @@ class MainGameScene extends Scene {
     });
 
     batarang.children.iterate(function (child) {
-      child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8));
+      child.setBounceY(Phaser.Math.FloatBetween(0.2, 0.4));
     })
 
     scoreText = this.add.text(16, 16, 'score: 0', { fontSize: '32px', fill: 'white' });
@@ -167,7 +186,7 @@ class MainGameScene extends Scene {
     this.cameras.main.setBounds(0, 0, 2822, 384);
     // make the camera follow the player
     this.cameras.main.startFollow(player);
-
+    
     
   }
 
